3a2cabc53a3efdacb4c0519d7b64a9ee29037e45
adong <byczd@163.com>
2023-05-09 17:36:44 +0800

    add.vpn.tunnel

--- NetworkExtentionDemo/VpnManager.swift
+++ NetworkExtentionDemo/VpnManager.swift
@@ -8,6 +8,10 @@
 import UIKit
 import NetworkExtension
 
+public enum ManagerError: Error {
+    case invalidProvider
+    case vpnStartFail
+}
 
 public enum VPNStatus {
     case off
@@ -77,6 +81,27 @@ class VpnManager{
     }
     
     
+    open func switchVPN(_ completion: ((NETunnelProviderManager?, Error?) -> Void)? = nil) {
+        loadProviderManager { [unowned self] (manager) in
+            if let manager = manager {
+                self.updateVPNStatus(manager)
+            }
+            let current = self.vpnStatus
+            guard current != .connecting && current != .disconnecting else {
+                return
+            } //只要不是off都关闭会怎么样
+            if current == .off {
+                self.startVPN(nil) { (manager, error) -> Void in
+                    completion?(manager, error)
+                }
+            }else {
+                self.stopVPN()
+                completion?(nil, nil)
+            }
+        }
+    }
+    
+    
 }
 
 extension VpnManager {
@@ -92,6 +117,103 @@ extension VpnManager {
             }
             complete(nil)
         }
+    }
+    
+    public func startVPN(_ options: [String : NSObject]?, complete: ((NETunnelProviderManager?, Error?) -> Void)? = nil) {
+        // Load provider
+        loadAndCreateProviderManager { (manager, error) -> Void in
+            if let error = error {
+                complete?(nil, error)
+            }else{
+                guard let manager = manager else {
+                    complete?(nil, ManagerError.invalidProvider)
+                    return
+                }
+                
+                if manager.connection.status == .disconnected || manager.connection.status == .invalid {
+                    do {
+//要拦截流量，需要主App启动Network Extension进程，
+//即通过调用NETunnelProviderManager对象tunnel的tunnel.connection.startVPNTunnel()方法。开启VPN通道
+                        try manager.connection.startVPNTunnel(options: options)
+                        self.addVPNStatusObserver()
+                        complete?(manager, nil)
+                    }catch {
+                        complete?(nil, error)
+                    }
+                }else{
+                    self.addVPNStatusObserver()
+                    complete?(manager, nil)
+                }
+            }
+        }
+    }
+    
+    public func stopVPN() {
+        // Stop provider
+        loadProviderManager { (manager) -> Void in
+            guard let manager = manager else {
+                return
+            }
+//stopVPNTunnel关闭VPN通道
+            manager.connection.stopVPNTunnel()
+        }
+    }
+    
+    fileprivate func loadAndCreateProviderManager(_ complete: @escaping (NETunnelProviderManager?, Error?) -> Void ) {
+        NETunnelProviderManager.loadAllFromPreferences { [unowned self] (managers, error) -> Void in
+// NETunnelProviderManager.loadAllFromPreferences,读取manager配置，
+// saveToPreferences如果调用多次，会出现VPN 1 VPN 2等多个描述文件，故苹果要求，在创建前应读取当前的managers。
+// 而loadAllFromPreferences获取，第一次肯定是nil，
+            
+            if let managers = managers {
+                let manager: NETunnelProviderManager
+                if managers.count > 0 {
+                    manager = managers[0]
+                }else{
+                    manager = self.createProviderManager()
+                }
+                manager.isEnabled = true
+                manager.localizedDescription = "我爱一条柴"
+                // 任意值,显示在设置VPN中的vpn名称，可以写成如： "我爱一条柴"
+                manager.protocolConfiguration?.serverAddress = "127.0.0.1"
+                // 任意值,显示在设置-VPN-Detial中的“服务器”值，可以写成如："上山打老虎"
+                manager.protocolConfiguration?.excludeLocalNetworks = true
+
+//-----按需连接(按需开启代理)配置，在vpn关闭的情况，如果监测到外网请求自动开启vpn；
+//                manager.isOnDemandEnabled = true //默认开关状态为false
+// isOnDemandEnabled按需启动，Toggles VPN On Demand. 按需启动开关
+//                let quickStartRule = NEOnDemandRuleEvaluateConnection()
+////// NEEvaluateConnectionRule将网络连接的属性与操作相关联
+////                let autoDomains = ["google.com","youtube.com","wikipedia.org","facebook.com","twitter.com","instagram.com","snapchat.com","voachinese.com","dw.com","bbc.com","rfi.fr","cnn.com","news.sky.com","cw.com","apnews.com","aljazeera.net","whatsapp.com","whatsapp.net"]
+////                "aljazeera.net" 半岛电视台
+//                quickStartRule.connectionRules = [NEEvaluateConnectionRule(matchDomains:["google.com","youtube.com"] , andAction: NEEvaluateConnectionRuleAction.connectIfNeeded)]
+//////              按需连接,如果开关开启，则在打开google.com时，会自动开启VPN(VPN关闭的情况下)
+//                manager.onDemandRules = [quickStartRule]
+                
+// saveToPreferences将manager保存至系统中, 如果saveToPreferences方法调用多次，会出现VPN 1 VPN 2等多个描述文件
+                manager.saveToPreferences(completionHandler: { (error) -> Void in
+                    if let error = error {
+                        complete(nil, error)
+                    }else{
+                        manager.loadFromPreferences(completionHandler: { (error) -> Void in
+                            if let error = error {
+                                complete(nil, error)
+                            }else{
+                                complete(manager, nil)
+                            }
+                        })
+                    }
+                })
+            }else{
+                complete(nil, error)
+            }
+        }
+    }
+    
+    fileprivate func createProviderManager() -> NETunnelProviderManager {
+        let manager = NETunnelProviderManager()
+        manager.protocolConfiguration = NETunnelProviderProtocol()
+        return manager
     }
     
 }
--- TunnelDemo/PacketTunnelProvider.m
+++ TunnelDemo/PacketTunnelProvider.m
@@ -6,11 +6,31 @@
 //
 
 #import "PacketTunnelProvider.h"
+#import "totoReachabe.h"
+
+#include <resolv.h>
+#import <arpa/inet.h>
+
+#define TunnelMTU 1600
+
+@interface PacketTunnelProvider(){
+    void (^_pendingStartCompletion)(NSError *);
+    void (^_pendingStopCompletion)(void);
+    
+    NWPath *_lastPath;
+    NetworkStatus _lastNetType;
+    NSInteger _iTunnelChange;
+}
+@end
 
 @implementation PacketTunnelProvider
 
+#pragma mark - NEPacketTunnelProvider
 - (void)startTunnelWithOptions:(NSDictionary *)options completionHandler:(void (^)(NSError *))completionHandler {
     // Add code here to start the process of connecting the tunnel.
+    _pendingStartCompletion = completionHandler;
+    _iTunnelChange=0;
+    [self startPacketForwarders];
 }
 
 - (void)stopTunnelWithReason:(NEProviderStopReason)reason completionHandler:(void (^)(void))completionHandler {
@@ -30,5 +50,144 @@
 - (void)wake {
     // Add code here to wake up.
 }
+
+
+#pragma mark - VPNConfig
+- (void)startPacketForwarders {
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onTun2SocksFinished) name:kTun2SocksStoppedNotification object:nil];
+    
+    __weak typeof(self) weakSelf = self;
+    [self applyTunnelSettings:^(NSError *error) {
+        __strong typeof(self) strongSelf = weakSelf;
+        if (error == nil) {
+// NEProvider 中存在属性public var defaultPath: NWPath? { get } 表明当前系统的网络请求路径
+// 故可以addObserver添加对defaultPath属性的监听，来得到例如发生 WiFi->4G 切换时的网络变化
+// 在observeValueForKeyPath里处理监听到的状态改变
+            [weakSelf addObserver:weakSelf forKeyPath:@"defaultPath" options:NSKeyValueObservingOptionInitial context:nil];
+        }
+        if (strongSelf->_pendingStartCompletion) {
+            strongSelf->_pendingStartCompletion(error);
+            strongSelf->_pendingStartCompletion = nil;//至此VPN创建成功OVER
+        }
+    }];
+}
+
+- (void) applyTunnelSettings:(void (^)(NSError *error))completionHandler {
+//  这里是真正设置vpn tunnel 网络参数的地方，主控制器中设置的仅为系统vpn设置中的展示名称
+//  首先新建一个NEPacketTunnelNetworkSettings对象，此对象是用来设置并建立vpn tunnel初始化对象
+//  提供的RemoteAddress即为我们要建立连接的远程服务器的地址
+    
+//从配置中读取DNS
+//    NSString *generalConfContent = [NSString stringWithContentsOfURL:[AppProfile sharedGeneralConfUrl] encoding:NSUTF8StringEncoding error:nil];
+//    NSDictionary *generalConf = [generalConfContent jsonDictionary];
+//    NSString *dns = generalConf[@"dns"];
+
+//分配给TUN接口的IPv4地址(198.19.0.1为虚拟ip)和网络掩码
+    NEIPv4Settings *ipv4Settings = [[NEIPv4Settings alloc] initWithAddresses:@[@"192.0.2.1"] subnetMasks:@[@"255.255.255.0"]];
+//@"192.0.2.1",对应VPN-Detail中显示的 “地址”, 可以随便填写"198.19.0.1"、"172.19.0.1"
+
+//  includedRoutes：指定哪些IPv4网络流量的路由将被路由到TUN接口，即vpn tunnel需要拦截包的地址,如需全部拦截则设置[NEIPv4Route defaultRoute]
+//  excludeRoute ： 设置不拦截哪些包的地址，不路由到tun，而是直接发给设备。
+    ipv4Settings.includedRoutes = @[[NEIPv4Route defaultRoute]]; //全部拦截
+// NEPacketTunnelNetworkSettings包含IP层隧道的IP网络设置
+    NEPacketTunnelNetworkSettings *settings = [[NEPacketTunnelNetworkSettings alloc] initWithTunnelRemoteAddress:@"127.0.0.1"];
+//@"192.0.2.2",对应VPN-Detail中显示的 “服务器地址” 198.19.0.2
+    settings.IPv4Settings = ipv4Settings;
+    settings.MTU = @(TunnelMTU); //MTU:最大传输单元,即每个packet最大的容量为1600,超出则会分包
+    NEProxySettings* proxySettings = [[NEProxySettings alloc] init];
+    NSInteger proxyServerPort = 8118;//8118端口通常用于Privoxy web代理软件
+    NSString *proxyServerName = @"localhost";
+    NSAssert(proxyServerPort > 0, @"proxyServerPort > 0");
+
+    proxySettings.HTTPEnabled = YES;
+    proxySettings.HTTPServer = [[NEProxyServer alloc] initWithAddress:proxyServerName port:proxyServerPort];
+    
+    proxySettings.HTTPSEnabled = YES;
+    proxySettings.HTTPSServer = [[NEProxyServer alloc] initWithAddress:proxyServerName port:proxyServerPort];
+    
+    proxySettings.excludeSimpleHostnames = YES;
+    settings.proxySettings = proxySettings;
+    
+    NSArray *dnsServers=@[@"8.8.8.8"];//简单点直接写死 //getSystemDnsServers(取系统设置的DNS)
+    NEDNSSettings *dnsSettings = [[NEDNSSettings alloc] initWithServers:dnsServers];
+
+    dnsSettings.matchDomains = @[@""];// 包含域字符串的字符串数组。如果此属性为非nil，则DNS设置将仅用于解析指定域中的主机名。
+// if blank don't use this DNS, use system; if "" then use this(  //如果空白不使用此DNS，请使用system；如果""，则使用此)
+
+    settings.DNSSettings = dnsSettings;
+    
+    [self setTunnelNetworkSettings:settings completionHandler:^(NSError * _Nullable error) {
+        if (completionHandler) {
+            completionHandler(error);//completionHandler(nil) 完成建立VPN连接
+//  在设置网络参数成功后必须显式调用completionHandler(nil)才能正常建立连接(官方API要求)
+        }
+    }];
+}
+
+//获取系统DNS设置
+-(NSArray *)getSystemDnsServers {
+//need.   #include <resolv.h> & libresolv.tbd
+    res_state res = malloc(sizeof(struct __res_state));
+    res_ninit(res);
+    NSMutableArray *servers = [NSMutableArray array];
+    for (int i = 0; i < res->nscount; i++) { //res->nscount = 3,不管iOS手动设置了多少个，最多只返回3个；如果系统DNS为自动，则
+        sa_family_t family = res->nsaddr_list[i].sin_family;
+        char str[INET_ADDRSTRLEN + 1]; // String representation of address
+        if (family == AF_INET) { // IPV4 address
+            inet_ntop(AF_INET, & (res->nsaddr_list[i].sin_addr.s_addr), str, INET_ADDRSTRLEN); //need.#import <arpa/inet.h>
+            str[INET_ADDRSTRLEN] = '\0';
+            NSString *address = [[NSString alloc] initWithCString:str encoding:NSUTF8StringEncoding];
+            if (address.length) {
+                [servers addObject:address];
+            }
+        }
+    }
+    res_ndestroy(res);
+    free(res);
+    return servers;
+}
+
+
+#pragma mark - Net-Change-observe
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"defaultPath"]) {
+////NWPathStatusSatisfied网络可用//
+        if (NWPathStatusSatisfied == self.defaultPath.status){
+            //wifi切换到4G时，会有2次observeValueForKeyPath通知，但第1次status=NWPathStatusUnsatisfied
+            //随后连接成功了，又会收到1次NWPathStatusSatisfied状态，如果切换到主AppUI，则会再收到一次NWPathStatusSatisfied状态
+            TotoReachabe *reach = [TotoReachabe reachabilityForInternetConnection];
+            NetworkStatus status = [reach currentReachabilityStatus];
+            NSLog(@"_lastNetType:%ld",(long)_lastNetType); //1.默认notReachable，2.首次wifi开启变成ReachableWifi，3.切换至4G，变成ReachableWWAN
+            if (!_lastPath) {
+                _lastPath = self.defaultPath;
+                _lastNetType = status;
+                _iTunnelChange = 0;
+            }else if (![self.defaultPath isEqualToPath:_lastPath]){
+                _lastPath=self.defaultPath;
+                if (!_lastNetType) {
+                    _lastNetType = status;
+                }else if(_lastNetType != status && _iTunnelChange>0){
+                    NSLog(@"_lastNetType:%ld,status:%ld",(long)_lastNetType,(long)status);
+                    _lastNetType = status;
+
+                    NSLog(@"received network change notifcation");
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                        [self applyTunnelSettings:nil]; //网络改变时，重新建立vpn连接
+//                 [self doWhenChangingNetState]; //重启代理
+                    });
+                }
+                else{
+                    _iTunnelChange = 1;
+                }
+            }
+        }
+        else{
+            _iTunnelChange = 1;
+        }
+        
+    }
+    
+}
+
 
 @end
